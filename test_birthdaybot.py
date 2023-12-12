5fbe42e7fd372e6f786e205a0af7e73de02cdadd
Brent Langston <brentley@oufan.com>
2023-12-12 09:40:47 -0600

    use the env variable for url

--- test_birthdaybot.py
+++ test_birthdaybot.py
@@ -8,11 +8,14 @@ class TestBirthdayBot(unittest.TestCase):
     def test_download_ics_with_valid_url(self, mock_get):
         mock_get.return_value.status_code = 200
         mock_get.return_value.content = b'Some ICS content'
-        ics_content = download_ics('http://example.com/ics')
+        ics_content = download_ics(ics_url)
         self.assertIsNotNone(ics_content)
 
     # Additional tests here...
 
 if __name__ == '__main__':
+    ics_url = os.getenv('ICS_URL')
+    if not ics_url:
+        raise ValueError("ICS URL not provided. Set the ICS_URL environment variable.")
     unittest.main()
 
